cbfb3754925702c6cd9b8dfb68a37172d09f900a
pratikbee <pratibee>
2023-10-10 16:58:51 +0530

    Added prettier formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import './styles.css';
+import './styles.css'
 import Image from '../assets/download.png'
 import Logo from '../assets/download.svg'
-const App = () => { 
-    
-    return <><h1>This is App {process.env.NODE_ENV} and { process.env.name}</h1>
-        <img src={Image} alt="React Logo" width={300} height={ 200} />
-        <img src={Logo} alt="svg" width={300} height={ 200} />
+const App = () => {
+  return (
+    <>
+      <h1>
+        This is App {process.env.NODE_ENV} and {process.env.name}
+      </h1>
+      <img src={Image} alt="React Logo" width={300} height={200} />
+      <img src={Logo} alt="svg" width={300} height={200} />
     </>
+  )
 }
 
-export default App;
+export default App
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,2 +1,2 @@
-declare module "*.png"  // decalarations for image in typescript
-declare module "*.svg" // decalarations for svg in typescript
+declare module '*.png' // decalarations for image in typescript
+declare module '*.svg' // decalarations for svg in typescript
